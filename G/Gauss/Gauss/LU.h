5e66dd6d224074ae6b4affa3ddb0a7b8a6143570
SpaceBirB <126468286+SpaceBirbMan@users.noreply.github.com>
2023-11-05 20:22:24 +0900

    LU завершён и проверен

--- G/Gauss/Gauss/LU.h
+++ G/Gauss/Gauss/LU.h
@@ -10,51 +10,44 @@ void LU(int inv)
 {
 	int n = 0;
 	cin >>  n;
-	matrix L(n, n), U(n, n), A(n, n);
+	matrix L(n, n), U(n, n), A(n, n+1);
 	matrix X(n), Y(n), B(n);
-
-
-
-	float sum1 = 0, sum2 = 0;
-
 	
 	L >> 0;
 	U >> 0;
 
 	A.randomize_advanced(1, 20);
 	
-	B = A += n;
+	B = A += n+1;
 
 	B << B;
 	A << A;
 
-	float f = L(1, 1);
 
 	{
 		
 		for (int i = 0; i < n; i++)
-		{
 			L(i, 0) = A(i, 0);
-			U(0, i) = A(0, i) / f;
-		}
+		for (int j = 1; j < n; j++)
+			U(0, j) = A(0, j) / L(0, 0);
 
 		for (int k = 1; k < n; k++)
 		{
-			float f2 = L(k, k);
-
 			for (int i = k; i < n; i++)
 			{
+				L(i, k) = A(i, k);
 				for (int m = 0; m < k; m++)
-					sum1 = sum1 + L(i, m) * U(m, k);
-				L(i, k) = A(i, k) - sum1;
+					L(i, k) = L(i, k) - L(i, m) * U(m, k);
 			}
 
-			for (int j = k + 1; j < n; j++)
-			{
-				for (int m = 0; m < k; m++)
-					sum2 = sum2 + L(k, m) * U(m, j);
-				U(k, j) = (1 / f2) * (A(k, j) - sum2);
-			}
+			if (k <= n - 1)
+				for (int j = k + 1 ; j < n; j++)
+				{
+					U(k, j) = A(k, j);
+					for (int m = 0; m < k; m++)
+						U(k, j) = U(k,j) - L(k, m) * U(m,j);
+					U(k, j) = U(k, j) / L(k, k);
+				}
 		}
 	}
 	
@@ -62,19 +55,29 @@ void LU(int inv)
 	U << U;
 
 	{
-		Y(0) = B(0) / f;
-
-		float sum = 0;
+		Y(0) = B(0) / L(0,0);
 
 		for (int i = 1; i < n; i++)
 		{
-			for (int m = 0; m < i - 1; m++)
-				sum = L(i,m)*Y(m);
-			Y(i) = (1 / L(i, i)) * (B(i) - sum);
+			Y(i) = B(i);
+			for (int m = 0; m < i; m++)
+				Y(i) = Y(i) - L(i, m) * Y(m);
+			Y(i) = Y(i)/L(i,i);
 		}
 	}
 
+	{
+		X(n - 1) = Y(n - 1);
 
+		for (int i = n - 1; i >= 0; i--)
+		{
+			X(i) = Y(i);
+			for (int m = i + 1; m < n; m++)
+				X(i) = X(i) - U(i, m) * X(m);
+		}
+	}
+
+	X << X;
 }
 //int i = n - 1;
 //
